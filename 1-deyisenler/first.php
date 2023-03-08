a0c7ec83840ccdae90d90b930ba1fae7f29a0650
aladdinalizada <tu7c5zneb@code.edu.az>
2023-03-09 00:20:46 +0400

    some additions

--- 1-deyisenler/first.php
+++ 1-deyisenler/first.php
@@ -1,6 +1,16 @@
 <?php
 // Dəyişənləri $ ilə qeyd edirik
- $ad = "Eleddin Elizade";
-//  localhostda görmək ücün echo-dan istifadə edirik
- echo $ad;
+// String
+ $ad = "Eleddin Elizade"; 
+// Number
+ $yas = 28;
+//  Array
+ $adlar = array("Ali", "Veli", "Fatma", "ALAddin");
+// Boolen
+// Boolen deyerler 1 ve 0 olaraq qeyd olunur N
+ $kontrol = true;
+ //  localhostda görmək ücün echo-dan istifadə 
+ echo "$ad $yas $adlar[0] $kontrol";
+//  eger bir nece deyisen istifade edirikse, ""-dan istifade edmeliyik
+
 ?>
